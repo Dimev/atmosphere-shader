93fd29c449b4a9cab8c9154b8074354a58e28d29
Dimas <skythedragon@outlook.com>
2021-07-20 19:58:51 +0200

    stuff happens in the calculation of the integrals, so we have data points

--- tools/optical_depth.py
+++ tools/optical_depth.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.integrate
+import math
 
 # we want to figure out a good approximation to the optical depth integral 
 # we can calculate this as the integral over beta * exp(-scale_height * (sqrt(t^2 + 2bt + c) - planet_radius)), from t = 0 to infinity 
@@ -20,6 +21,29 @@ def optical_depth(b, c, scale_height, planet_radius):
 i = 0
 
 data = []
+
+# loop over all possible points
+# b is basically -inf to inf
+# c is - planet_radius^2 to inf
+# scale height is can be -inf to inf, but more realistically 0 to inf
+# planet radius is 0 to inf
+# for inf, we'll just use 1e6
+# big enough
+for b in np.arange(-10, 10, 1):
+	for c in np.arange(-10, 10, 1):
+		for scale_height in np.arange(0.0, 10, 1):
+			for planet_radius in np.arange(0.0, 10, 1):
+
+				# TODO: skip when the inside of the sqrt can become smaller than 0
+
+				val = optical_depth(b, c, scale_height, planet_radius)[0]
+
+				if not math.isnan(val):
+
+					data.append((b, c, scale_height, planet_radius, val))
+
+print(len(data))
+
 
 
 
